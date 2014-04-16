86dfe4c6c498979fe8275fe180645ff3bb535cc7
Philip Lee <leephili@usc.edu>
2014-04-16 14:10:42 -0700

    Cleaned up and explained some stuff

--- Blue/content.py
+++ Blue/content.py
@@ -1,12 +1,9 @@
 import subprocess as sub
 
-interface = 'eth0';
+interface = 'eth0'
+arguments = '-l -i ' + interface + ' -A -s 1500 dst port 80' #-l makes line breaks, -i for interface, -A for ASCII print out, -s for size (make 1500 so it doesn't cut out), dst port 80 for http incoming requests
+p = sub.Popen(('sudo', 'tcpdump', arguments), stdout=sub.PIPE) #Subprocess tcpdump pipe output to this python script
 
-arguments = '-l -i  -A -s 1500 dst port 80';
-
-p = sub.Popen(('sudo', 'tcpdump', arguments), stdout=sub.PIPE)
 try:
     for row in p.stdout:
         print row.rstrip()   # process here
-except KeyboardInterrupt:
-    p.terminate()            # zombie protection, if needed
